ea02ade983348617c2946ab0f602b16ff64b0119
saragalal <galalsara0@gmail.com>
2022-01-04 16:46:27 +0200

    displaying a database

---
+++ showdb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear
+echo "===================="
+echo "Available Databases:"
+ls -1 ./Databases
+echo "===================="
+
+typeset -i flag3
+flag3=0
+while [[ $flag3 -eq 0 ]]
+do
+echo -e "For Main Menu, Press 0"
+read answer
+case $answer in 
+	0) let flag3++ 
+	. ./main.sh
+		;;
+	*) echo "Invalid Option!"
+		;;
+esac
+done
+let flag3=0
