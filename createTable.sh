1983124ee97048cfa1ea9c1cc6e78d833c7def84
Zainab Sabry <zainab.hamd@gmail.com>
2022-01-05 13:00:41 +0200

    Creating a table

---
+++ createTable.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+while true 
+do
+echo -e "1- Press 1 to proceed with creating the table\n2- Press 2 to go back to the previous menu"
+echo "Please choose one of the option: "
+read choice
+clear
+	case $choice in 
+	1) . ./cols.sh
+		break
+			;;
+	2) . ./second.sh
+			;;
+	*) clear 
+		echo "Invalid option"
+			;;
+	esac
+done
+
