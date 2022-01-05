58f5484f2181544583bea3f14964b1ee96d25877
Zainab Sabry <zainab.hamd@gmail.com>
2022-01-05 20:39:35 +0200

    Creating a Database

--- createdb.sh
+++ createdb.sh
@@ -2,44 +2,47 @@
 clear
 typeset -i flag
 flag=0
-while true #"Press 1 to enter the name of the database" "Press 2 to go back to the Main Menu"
+while [[ flag -eq 0 ]]
 do
-echo -e "1- Press 1 to enter database name\n2- Press 2 to go back to Main Menu"
-echo "Please choose one of the option: "
-read answer
-clear
-	case $answer in 
-	1) while [[ flag -eq 0 ]]
-		do
-		echo "Please enter the name of the database, press 0 for Main Menu"
-		read databaseName
-	   		case $databaseName in
-				+([a-zA-Z_]*[a-zA-Z0-9_]))
-					if [ -d ./Databases/$databaseName ]
-					then 
-						echo "This database already exists"
-					else
-						let flag++
-						clear
-						echo "======================================"
-						echo "$databaseName database created successfully!"
-						echo "======================================"
-						mkdir -p ./Databases/$databaseName	
-						break
-					fi
+	echo "Please enter the name of the database, press 0 for Main Menu"
+	read databaseName
+	   	case $databaseName in
+			+([a-zA-Z_]*[a-zA-Z0-9_]))
+				if [ -d ./Databases/$databaseName ]
+				then 
+					echo "This database already exists"
+				else
+					let flag++
+					clear
+					echo "==================================================="
+					echo "$databaseName database created successfully!"
+					echo "==================================================="
+					mkdir -p ./Databases/$databaseName	
+					sleep 3
+					clear
+					#What to do next
+					while true 
+					do
+						echo -e "1- Create Another Database\n2- Go Back to Main Menu"
+						echo "Please choose one of the options:"
+						read option
+							case $option in
+								1). ./createdb.sh
+									;;
+								2) . ./main.sh
+									;;
+								*) clear
+									echo "Invalid Option!"
+									;;
+							esac
+					done
+				fi
 							;;
-				0) . ./main.sh
+			0) . ./main.sh
 							;;
-				*) echo "invlid name! Please choose another name"
+			*) clear
+				echo "invlid name! Please choose another name"
 							;;
-			esac
-		done
-		let flag=0
-			;;
-	2) . ./main.sh
-			;;
-	*) clear 
-		echo "Invalid option"
-			;;
-	esac
+esac
 done
+
