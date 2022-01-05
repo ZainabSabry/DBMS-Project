51762542a73ca7b5498192644416dcc1d4524d80
Zainab Sabry <zainab.hamd@gmail.com>
2022-01-05 21:46:37 +0200

    Dropping a Table

--- dropTable.sh
+++ dropTable.sh
@@ -9,22 +9,23 @@ then
 	case $option in
 	*) . ./second.sh
 		;;
-	esac
-#If there are tables, then list them and choose which to drop
-#List the available ones	
+	esac	
 else
+
+while true
+do
+#If there are tables, then list them and choose which to drop
+#List the available ones
 echo "===================="
 echo "Available Tables:"
 ls -1 ./Databases/$dbname
 echo "===================="
 
 #Ask the user to enter the name of the table
-while true
-do
 echo  "Please enter the table name, For Previous Menu, Press 0 "
 read tableName
 	case $tableName in
-		+([a-zA-Z_]*[a-zA-Z0-9_]))	
+		+([a-zA-Z_]*[a-zA-Z0-9_]))	clear
 			if [ -f ./Databases/$dbname/$tableName ]
 			then 
             clear
@@ -33,28 +34,49 @@ read tableName
 				echo "Are you sure you want to delete $tableName table! [yes|no]?"
 				read areYouSure
 				case ${areYouSure,,} in
-					"yes" | "y") rm  ./Databases/$dbname/$tableName
+					"yes" | "y") clear 
+								rm  ./Databases/$dbname/$tableName
                                 clear
 								echo "$tableName is Successfully Deleted!"
 								echo "===================="
 								echo "Available Tables:"
 								ls -1 ./Databases/$dbname
 								echo "===================="
-                                break
+                                sleep 3
+								clear
+								#What to do next
+								while true 
+								do
+									echo -e "1- Drop Another Table\n2- Go Back to Previous Menu"
+									echo "Please choose one of the options:"
+									read option
+										case $option in
+											1). ./dropTable.sh
+												;;
+											2) . ./second.sh
+												;;
+											*) clear
+												echo "Invalid Option!"
+												;;
+										esac
+								done
 										;;
 				    "no" | "n") . ./dropTable.sh
 										;;
-						*) echo "Invalid Option!"
+						*) clear
+							echo "Invalid Option!"
 										;;
 				esac
                 done
 			else
+			clear
 				echo "This table does not exist!"
 			fi
 							;;
 		0) . ./second.sh
 							;;
-		*) echo "Invalid input!"
+		*) clear
+			echo "Invalid input!"
 							;;
 	esac
 
